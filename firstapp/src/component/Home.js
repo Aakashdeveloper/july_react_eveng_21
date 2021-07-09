10a863334762d186aab3b4775de65b7c4b5a444c
aakashdeveloper <ahanda205@gmail.com>
2021-07-09 19:47:05 +0530

    classComponent

---
+++ firstapp/src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer'
+
+const Home = () => {
+    return(
+        <>
+            <Header/>
+            <h1>Hii to React App</h1>
+            <h2>My App</h2>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home;
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,26 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
-// var React = require('react');
-
-/*function App(){
-    return(
-        <div>
-            <Header/>
-            <h1>Hii to React App</h1>
-            <h2>My App</h2>
-        </div> 
-    )
-}*/
-const App = () => {
-    return(
-        <div>
-            <Header/>
-            <h1>Hii to React App</h1>
-            <h2>My App</h2>
-        </div>
-    )
-}
-
-// here connected with index.html
-ReactDOM.render(<App/>,document.getElementById('root'));
+import Home from './component/Home'
+ReactDOM.render(<Home/>,document.getElementById('root'));
