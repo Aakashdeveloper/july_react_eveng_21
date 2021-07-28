01ed798c58410cf1df387146ff739bf43149a9c1
aakashdeveloper <ahanda205@gmail.com>
2021-07-28 19:39:34 +0530

    paymentgateway

--- thirdapp/src/component/Booking/placeBooking.js
+++ thirdapp/src/component/Booking/placeBooking.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-
-const url = "";
+const url = "http://localhost:8900/bookings";
 
 class PlaceBooking extends Component {
     constructor(props){
@@ -22,6 +21,15 @@ class PlaceBooking extends Component {
 
     handleSubmit = () => {
         console.log(this.state)
+        fetch(url,{
+            method: 'POST',
+            headers: {
+                "Accept": 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        })
+        .then(console.log('order placed'))
     }
 
     render(){
@@ -32,40 +40,42 @@ class PlaceBooking extends Component {
                         Place Booking
                     </div>
                     <div className="panel-body">
-                        <div className="form-group">
-                            <label>Order Id</label>
-                            <input readonly name="id" value={this.state.id}
-                            className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label>Hotel Name</label>
-                            <input readonly name="hotel_name" value={this.state.hotel_name}
-                            className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label>Cost</label>
-                            <input readonly name="cost" value={this.state.cost}
-                            className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label>Name</label>
-                            <input name="name" value={this.state.name}
-                            className="form-control" onChange={this.handleChange}/>
-                        </div>
-                        <div className="form-group">
-                            <label>Email</label>
-                            <input name="email" value={this.state.email}
-                            className="form-control" onChange={this.handleChange}/>
-                        </div>
-                        <div className="form-group">
-                            <label>Phone</label>
-                            <input name="phone" value={this.state.phone}
-                            className="form-control" onChange={this.handleChange}/>
-                        </div>
-                        <button className="btn btn-success" type="submit"
-                        onClick={this.handleSubmit}>
-                            Place Booking
-                        </button>
+                        <form method='POST' action="https://developerpayment.herokuapp.com/paynow">
+                            <div className="form-group">
+                                <label>Order Id</label>
+                                <input readonly name="id" value={this.state.id}
+                                className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Hotel Name</label>
+                                <input readonly name="hotel_name" value={this.state.hotel_name}
+                                className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Cost</label>
+                                <input readonly name="cost" value={this.state.cost}
+                                className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input name="name" value={this.state.name}
+                                className="form-control" onChange={this.handleChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input name="email" value={this.state.email}
+                                className="form-control" onChange={this.handleChange}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Phone</label>
+                                <input name="phone" value={this.state.phone}
+                                className="form-control" onChange={this.handleChange}/>
+                            </div>
+                            <button className="btn btn-success" type="submit"
+                            onClick={this.handleSubmit}>
+                                Place Booking
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
